8331a16bf2ed70736fca90347b1aa128388d8c41
Daan Dekoning Krekels <daan.dekoningkrekels@student.ap.be>
2021-11-28 21:51:42 +0100

    Fietsstation objecten toegevoegd

--- Programma.py
+++ Programma.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from FietsSysteem import *
 
 oFietsStation1 = FietsStation("Centraal Station - Astrid", 1, 32, "51.21798654307154", "4.420624304044165")
-oFietsStation1 = FietsStation("Centraal Station - Astrid", 1, 32, "51.21798654307154", "4.420624304044165")
+oFietsStation2 = FietsStation("Centraal Station - Astrid 2", 2, 32, "51.21771096036532", "4.420667211070502")
+oFietsStation3 = FietsStation("Centraal Station - Kievit 2", 3, 32, "51.213237097511296", "4.421803495141187")
 oFietsStation1.voeg_fiets_toe(
     Fiets(1, 3),
     Fiets(2, 2)
